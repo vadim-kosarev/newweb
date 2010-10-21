45942f3f2023e4f84fa1c855a55e0f0a65a79cbf
vadim.kosarev <vadim.kosarev@0d206ae4-2e01-11df-9924-27d7866ff452>
2010-10-21 05:35:23 +0000

    Simple Filter

--- trunk/Web/GenStater/Dashboard/www/web_query.php
+++ trunk/Web/GenStater/Dashboard/www/web_query.php
@@ -1,6 +1,9 @@
 <?php
 include_once("sys_config.php");
-$dbh = new PDO('mysql:host=' . $config_db_host . ';dbname=' . $config_db_name, $config_db_user, $config_db_password);
+include_once("sys_utils.php");
+
+$dbUrl = 'mysql:host=' . $config_db_host . ';dbname=' . $config_db_name;
+$dbh = new PDO($dbUrl, $config_db_user, $config_db_password);
 
 $queryID = 1;
 if (isset($_GET["id"]))
@@ -11,27 +14,95 @@ $stmt->bindValue(":queryID", $queryID);
 
 $qSQL = "";
 $qName = "";
+$qOrderBy = "";
 
 if ($stmt->execute()) {
     $dArr = $stmt->fetch();
     $qSQL = $dArr["sql"];
     $qName = $dArr["name"];
+    $qOrderBy = $dArr["orderby"];
 }
 ?>
-<div class="sqlTableHeader"><?= $qName ?></div>
-<div class="sql"><?= $qSQL ?></div>
 
+<div class="sqlTableHeader"><?= $qName ?></div>
+
+<?php
+// populate with GET parameters
+$whereClause = "";
+$whereClauseEmpty = true;
+foreach (array_keys($_GET) as $key) {
+    $arr = array();
+    if (preg_match("/(sql_)(.+)/i", $key, $arr)) {
+        if (!isset($_GET[$key]) || $_GET[$key] == "")
+            continue;
+        $val = $_GET[$key];
+        $p = paramGetToSQL($arr[2]);
+        $p = findFieldByAlias($p, $qSQL);
+        if ($whereClauseEmpty) {
+            $whereClause .= " WHERE \n";
+        }
+        if (!$whereClauseEmpty) {
+            $whereClause .= " AND \n";
+        }
+        $clause = " = ";
+        if (strpos($val, "*") === false) {
+        } else {
+            $val = str_replace("*", "%", $val);
+            $clause = " LIKE ";
+        }
+        $whereClause .= $p . " $clause '" . $val . "'";
+        $whereClauseEmpty = false;
+    }
+}
+
+// ORDER BY
+$orderByAdd = "";
+if (strlen($qOrderBy) > 0) {
+    $orderByAdd = " ORDER BY " . $qOrderBy;
+}
+
+// LIMIT
+$limitAdd = " LIMIT 0,1000 ";
+
+
+$qSQL = $qSQL . $whereClause . "\n" . $orderByAdd . "\n" . $limitAdd;
+?>
+
+<?php
+$stmt = $dbh->prepare($qSQL);
+
+// execute and serialize results
+$odd = 1;
+if ($stmt->execute()) {
+    $cCount = $stmt->columnCount();
+?>
+<table>
+<tr>
+    <td>
+    <form action="<?= $_SERVER['REQUEST_URI'] ?>">
+        <input type="hidden" name="id" value="<?=$queryID?>"/>
+        <table>
+        <?php
+        for ($i = 0; $i < $cCount; $i++) {
+            $cMeta = $stmt->getColumnMeta($i);
+            $cMetaName = $cMeta["name"];
+            $formParamName = "sql_$cMetaName";
+            $formParamValue = "";
+            if (isset($_GET[$formParamName])) {
+                $formParamValue = $_GET[$formParamName];
+            }
+            echo "<tr><td>" . $formParamName . "</td>";
+            echo "<td><input name='$formParamName' type='text' value='$formParamValue'/></td></tr>";
+        }
+        ?>  </table>
+    <input type="submit"/>
+</form>
+</td>
+<td><pre class="sql"><?= $qSQL ?></pre></td>
+</tr></table>
 <table class="sqlData">
     <?php
-    $stmt = $dbh->prepare($qSQL);
-    foreach (array_keys($_GET) as $key) {
-        $stmt->bindParam(":" . $key, $_GET[$key]);
-    }
-    $odd = 1;
-    if ($stmt->execute()) {
-
         // columns headers
-        $cCount = $stmt->columnCount();
         echo "<tr class='sqlDataHeader'>\n";
         for ($i = 0; $i < $cCount; $i++) {
             $cMeta = $stmt->getColumnMeta($i);
