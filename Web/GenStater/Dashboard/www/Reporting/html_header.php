af41615a8b7f1ab475d4a58f5396886a795388e9
vadim.kosarev@gmail.com <vadim.kosarev@gmail.com@0d206ae4-2e01-11df-9924-27d7866ff452>
2010-12-26 12:22:06 +0000

    Filter form is hidden by default

--- Web/GenStater/Dashboard/www/Reporting/html_header.php
+++ Web/GenStater/Dashboard/www/Reporting/html_header.php
@@ -1,7 +1,38 @@
 <html>
-    <head>
-        <title>HEAD</title>
-        <link href="style.css" rel="stylesheet" type="text/css">
-    </head>
-    <body>
+<head>
+<title>HEAD</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+<script language="javascript">
+<!--
 
+var state = 'none';
+
+function getNewState(oldState) {
+	if (oldState == 'block') {
+		return 'none';
+	} else {
+		return 'block';
+	}	
+}
+
+function showhide(layer_ref) {
+	
+	if (document.all) { //IS IE 4 or 5 (or 6 beta)
+		dispVal = "document.all." + layer_ref + ".style.display";  
+		eval( dispVal + " = getNewState(" + dispVal + ")");
+	}
+	
+	if (document.layers) { //IS NETSCAPE 4 or below
+		document.layers[layer_ref].display = getNewState(document.layers[layer_ref].display);
+	}
+	
+	if (document.getElementById && !document.all) {
+		hza = document.getElementById(layer_ref);
+		hza.style.display = getNewState(hza.style.display);
+	}
+	
+}
+//-->
+</script>
+</head>
+<body>
