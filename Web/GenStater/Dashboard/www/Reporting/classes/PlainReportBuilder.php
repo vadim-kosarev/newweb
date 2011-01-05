792b11bcc450f9ae42db3ad3d88668a065d5d67b
vadim.kosarev@gmail.com <vadim.kosarev@gmail.com@0d206ae4-2e01-11df-9924-27d7866ff452>
2011-01-05 09:25:24 +0000

    Classes for reporting

---
+++ Web/GenStater/Dashboard/www/Reporting/classes/PlainReportBuilder.php
@@ -0,0 +1,95 @@
+<?php
+include_once("DefaultReportBuilder.php");
+class PlainReportBuilder extends DefaultReportBuilder {
+
+
+	/**
+	 * (non-PHPdoc)
+	 * @see DefaultReportBuilder::processStmt()
+	 */
+	public function processStmt($stmt, $dArr) {
+		$this->printDataTable($stmt, $dArr);
+	}
+
+	
+	
+	
+	
+	/**
+	 * (non-PHPdoc)
+	 * @see DefaultReportBuilder::printDataHeader()
+	 */
+	public function printDataHeader($stmt, $dArr) {
+		?>
+<style>
+<!--
+
+div.category0 {
+}
+
+div.category1 {
+    border-top: 1px solid black;
+    padding-top: 10pt;
+	margin-top: 10pt;
+}
+
+div.category2 {
+	margin-top: 10pt;
+	margin-bottom: 5pt;
+	margin-left: 20pt;
+}
+
+-->
+</style>		
+		<?php 
+	}
+
+	
+	
+	/**
+	 * (non-PHPdoc)
+	 * @see DefaultReportBuilder::printDataFooter()
+	 */
+	public function printDataFooter($stmt, $dArr) {
+
+	}
+
+	
+	
+	/**
+	 * (non-PHPdoc)
+	 * @see DefaultReportBuilder::printPageHeader()
+	 */
+	public function printPageHeader($stmt, $dArr) {
+			
+	}
+	
+	/**
+	 * (non-PHPdoc)
+	 * @see DefaultReportBuilder::printDataRow()
+	 */
+	public function printDataRow($stmt, $dArr, $row) {
+		$cCount = $stmt->columnCount();
+		for ($i = 0; $i < $cCount; $i++) {
+			$v = trim($row[$i]);
+			if ( $i < $this->categoryNColumns ) {
+				
+				if ($this->catValues[$i] != $v) {
+					
+					$this->catValues[$i] = $v;
+					for ($j = $i+1 ; $j < $this->categoryNColumns; $j++) $this->catValues[$j] = null;
+					
+					echo "<div class='category$i'>$v</div>";
+				}
+				
+			} else {
+				echo " $v ";
+			}
+		}
+		echo "<br/>\n";
+
+	}
+
+
+}
+?>
