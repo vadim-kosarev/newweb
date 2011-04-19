71ad05a410440e838ae5dac91747bbcc7cea9366
vadim.kosarev@gmail.com <vadim.kosarev@gmail.com@0d206ae4-2e01-11df-9924-27d7866ff452>
2011-04-19 12:30:08 +0000

    added parameter dBase for getExtDataID

--- Web/GenStater/Dashboard/www/Collector/web_getExtDataID.php
+++ Web/GenStater/Dashboard/www/Collector/web_getExtDataID.php
@@ -6,9 +6,12 @@ if (!array_key_exists("dataTable", $_GET)) {
 	exit(0);
 } else {
 	include_once("class_event.php");
+	$dBase = 10;
+	if (array_key_exists("dBase", $_GET)) $dBase = $_GET["dBase"];
 	$event = new Event();
 	$dataTable = $_GET["dataTable"];
 	$dStrName =  $_GET["dStrName"];
-	echo $event->getExtDataID($dataTable, $dStrName, true);
+	$result = $event->getExtDataID($dataTable, $dStrName, true);
+	echo base_convert($result, 10, $dBase);
 }
 ?>
