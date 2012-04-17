c9cf4633ba819ef3725526443243da31a4ace599
vadim.kosarev@gmail.com <vadim.kosarev@gmail.com@0d206ae4-2e01-11df-9924-27d7866ff452>
2012-04-17 19:57:51 +0000

    Added sys_queries.sql

---
+++ Web/GenStater/Database/sys_queries.sql
@@ -0,0 +1,17 @@
+# --------------------------------------------------------
+# Host:                         127.0.0.1
+# Database:                     gen_stat
+# Server version:               5.1.51-community
+# Server OS:                    Win64
+# HeidiSQL version:             5.0.0.3272
+# Date/time:                    2012-04-17 23:57:10
+# --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+# Data exporting was unselected.
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
